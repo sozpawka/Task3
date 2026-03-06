8af15ac5b5106410fdfe63d1585e5a6bd693f04f
Sozpawka <sozpawkawar@gmail.com>
2026-03-06 21:40:14 +0700

    3_commit_функционал_модального_окна

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@ new Vue({
 	el:'#app',
 	data:{
 		showModal:false,
+		editIndex:null,
 		tasks:{
 			planned:[],
 			progress:[],
 			testing:[],
 			done:[]
 		},
-
 		newTask:{
 			title:'',
 			description:'',
-			deadline:''
+			deadline:'',
+			createdAt:'',
+			updatedAt:''
 		}
 	},
 	methods:{
@@ -21,7 +23,40 @@ new Vue({
 		},
 		closeModal(){
 			this.showModal=false
-		}
+			this.resetTask()
+		},
+		createTask(){
+			if(!this.newTask.title) return
 
+			const now=new Date().toLocaleString()
+
+			if(this.editIndex===null){
+				this.newTask.createdAt=now
+				this.tasks.planned.push({...this.newTask})
+			}else{
+				this.newTask.updatedAt=now
+				this.$set(this.tasks.planned,this.editIndex,{...this.newTask})
+			}
+
+			this.closeModal()
+		},
+		editTask(index){
+			this.editIndex=index
+			this.newTask={...this.tasks.planned[index]}
+			this.showModal=true
+		},
+		deleteTask(index){
+			this.tasks.planned.splice(index,1)
+		},
+		resetTask(){
+			this.editIndex=null
+			this.newTask={
+				title:'',
+				description:'',
+				deadline:'',
+				createdAt:'',
+				updatedAt:''
+			}
+		}
 	}
 })
